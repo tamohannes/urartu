24b19fc084658da56b9b368366282a127fb9f722
Hovhannes Tamoyan <hovhannes.tamoyan@gmail.com>
2024-06-26 21:40:48 +0200

    [fix] workflow config fix

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     version=VERSION,
     description=DESCRIPTION,
     long_description=long_description,
-    long_description_content_type='text/markdown'
+    long_description_content_type='text/markdown',
     python_requires=REQUIRES_PYTHON,
     install_requires=requirements,
     packages=find_packages("."),
