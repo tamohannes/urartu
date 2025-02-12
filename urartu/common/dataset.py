16087171f41ef33e5e9b1afbbd7dda7821aeb959
Hovhannes Tamoyan <tamoyan@slurm-login.ukp.informatik.tu-darmstadt.de>
2025-02-12 20:39:30 +0100

    [feat] Make dataloader padd to the batch max len

--- urartu/common/dataset.py
+++ urartu/common/dataset.py
@@ -2,8 +2,12 @@ from typing import Any, Dict, List
 
 import hydra
 from torch.utils.data import DataLoader
+import logging
 
 from transformers import DataCollatorWithPadding
+
+# Configure logging
+logging.basicConfig(level=logging.WARNING)
 
 
 class Dataset:
@@ -21,11 +25,21 @@ class Dataset:
 
     def get_dataloader(self, dataloader_cfg: Dict[str, Any], tokenizer):
         def collate_fn(examples):
+            max_input_size = max(len(example[dataloader_cfg["input_key"]]) for example in examples)
+            max_length = min(max_input_size, tokenizer.model_max_length)
+
+            for example in examples:
+                example_length = len(example[dataloader_cfg["input_key"]])
+                if example_length > tokenizer.model_max_length:
+                    logging.warning(
+                        f"Example input length {example_length} exceeds tokenizer.model_max_length {tokenizer.model_max_length}."
+                    )
+
             tokenized = tokenizer(
                 [example[dataloader_cfg["input_key"]] for example in examples],
                 truncation=True,
-                padding=True,  # Pad to the maximum length in the batch
-                max_length=dataloader_cfg.get("max_length", tokenizer.model_max_length),
+                padding=True,
+                max_length=max_length,
                 return_tensors="pt",
             )
 
@@ -39,7 +53,7 @@ class Dataset:
             self.dataset,
             shuffle=dataloader_cfg.get("shuffle", False),
             batch_size=dataloader_cfg.get("batch_size", 8),
-            num_workers=dataloader_cfg.get("num_workers", 4),
+            num_workers=dataloader_cfg.get("num_workers", 2),
             pin_memory=True,
             collate_fn=collate_fn,
         )
