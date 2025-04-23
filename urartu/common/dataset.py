3388e0fabaf2bdb65ba4f7ce0bd399e7c3eac54b
tamohannes <hovhannes.tamoyan@tu-darmstadt.de>
2025-04-23 21:07:07 +0200

    [feat] Remove right padding for tokenization

--- urartu/common/dataset.py
+++ urartu/common/dataset.py
@@ -87,7 +87,6 @@ class Dataset:
                 if example_length > tokenizer.model_max_length:
                     logging.warning(f"Example input length {example_length} exceeds tokenizer.model_max_length {tokenizer.model_max_length}.")
 
-            tokenizer.padding_side = "right"
             tokenized = tokenizer(
                 [example[dataloader_cfg["input_key"]] for example in examples],
                 truncation=True,
